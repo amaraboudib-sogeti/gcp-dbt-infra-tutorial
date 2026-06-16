2e7424c996ddbf5b9ac64c8ec4fe60dacbf140dd
Amara BOUDIB <amara.boudib@sogeti.com>
2026-06-16 16:05:32 +0200

    resolve conflicts

--- modules/gcs/main.tf
+++ modules/gcs/main.tf
@@ -1,12 +1,5 @@
 resource "google_storage_bucket" "raw" {
   name     = "${var.project_id}-raw"
   location = var.region
-<<<<<<< HEAD
-
-  versioning {
-    enabled = true
-  }
-=======
   project  = var.project_id
->>>>>>> dev
 }
--- modules/workflows/main.tf
+++ modules/workflows/main.tf
@@ -1,10 +1,7 @@
 resource "google_workflows_workflow" "pipeline" {
   name            = "data-pipeline"
   region          = var.region
-<<<<<<< HEAD
-=======
   project         = var.project_id
->>>>>>> dev
   service_account = var.service_account
 
   source_contents = file("${path.module}/workflow.yaml")
